a2dbacf8b067c30f6bb3bbbba2f7868ce235bb27
RoseSmyth <rosmyth23@gmail.com>
2026-03-02 18:25:25 -0500

    area function added

--- calculateArea/calculate_area.js
+++ calculateArea/calculate_area.js
@@ -9,3 +9,14 @@ function calculateArea() {
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function calculateTotalAmt() {
+    amt1 = parseFloat(document.getElementById('amt1').value);
+    amt2 = parseFloat(document.getElementById('amt2').value);
+    amt3 = parseFloat(document.getElementById('amt3').value);
+
+    let totalAmt = amt1 + amt2 + amt3;
+
+    document.getElementById('results').innerText = `The total amount it: ${totalAmt}`;
+}
+
